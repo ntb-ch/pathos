039dc63d58670cd0a40bde5b057ffc50b8d0d7dc
Claudia Visentin <claudia.visentin@ntb.ch>
2016-04-20 16:27:16 +0200

    canopen folder added

---
+++ canopen/include/canopen-faulhaber-drv.h
@@ -0,0 +1,18 @@
+#ifndef _CANOPEN_FAULHABER_DRV_H_
+#define _CANOPEN_FAULHABER_DRV_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int init_can_nodes(int sock);
+int init_faulhaber_motor(int sock, int node);
+int homing_faulhaber_motor(int sock, int node);
+int set_ramp_faulhaber(int sock, int node, int speed);
+int set_max_speed_faulhaber(int sock, int node, int speed);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CANOPEN_FAULHABER_DRV_H_ */
